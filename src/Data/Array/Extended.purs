e7e4b15473fe0ed45687168d0239b14e5e9fc5e4
Jürgen (Jutaro) Nicklisch-Franken <jnf@arcor.de>
2015-02-23 10:16:54 +0100

    Added comment to slice

--- src/Data/Array/Extended.purs
+++ src/Data/Array/Extended.purs
@@ -15,6 +15,10 @@
 
 module Data.Array.Extended where
 
+-- The slice method returns the selected elements in an array, as a new array object.
+-- It selects the elements starting at the given (1based) start argument, and ends at,
+-- but does not include, the given end argument.
+
 foreign import slice
   "function slice (s) {\
   \  return function (e) {\
