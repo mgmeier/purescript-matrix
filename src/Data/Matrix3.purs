d851d23b8babc2c32869a5d501d51d70cd82f7ed
Alex Mouton <thomas.mouton@gmail.com>
2017-12-07 21:22:18 -0800

    renamed det invDet.

--- src/Data/Matrix3.purs
+++ src/Data/Matrix3.purs
@@ -33,21 +33,21 @@ identity = Mat
 
 normalFromMat4 :: Mat Four Number -> Maybe Mat3
 normalFromMat4 (Mat [a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33]) =
-  if (det_ == 0.0)
+  if (det == 0.0)
         then Nothing
-        else let det = 1.0 / det_
+        else let invDet = 1.0 / det
               in Just $ Mat [
-                      (a11 * b11 - a12 * b10 + a13 * b09) * det,
-                      (a12 * b08 - a10 * b11 - a13 * b07) * det,
-                      (a10 * b10 - a11 * b08 + a13 * b06) * det,
+                      (a11 * b11 - a12 * b10 + a13 * b09) * invDet,
+                      (a12 * b08 - a10 * b11 - a13 * b07) * invDet,
+                      (a10 * b10 - a11 * b08 + a13 * b06) * invDet,
 
-                      (a02 * b10 - a01 * b11 - a03 * b09) * det,
-                      (a00 * b11 - a02 * b08 + a03 * b07) * det,
-                      (a01 * b08 - a00 * b10 - a03 * b06) * det,
+                      (a02 * b10 - a01 * b11 - a03 * b09) * invDet,
+                      (a00 * b11 - a02 * b08 + a03 * b07) * invDet,
+                      (a01 * b08 - a00 * b10 - a03 * b06) * invDet,
 
-                      (a31 * b05 - a32 * b04 + a33 * b03) * det,
-                      (a32 * b02 - a30 * b05 - a33 * b01) * det,
-                      (a30 * b04 - a31 * b02 + a33 * b00) * det]
+                      (a31 * b05 - a32 * b04 + a33 * b03) * invDet,
+                      (a32 * b02 - a30 * b05 - a33 * b01) * invDet,
+                      (a30 * b04 - a31 * b02 + a33 * b00) * invDet]
   where
     b00 = a00 * a11 - a01 * a10
     b01 = a00 * a12 - a02 * a10
@@ -62,6 +62,6 @@ normalFromMat4 (Mat [a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23,
     b10 = a21 * a33 - a23 * a31
     b11 = a22 * a33 - a23 * a32
       -- Calculate the determinant
-    det_ = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06
+    det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06
 
 normalFromMat4 _ = fail "Matrix4>>normalFromMat4: Impossible!"
