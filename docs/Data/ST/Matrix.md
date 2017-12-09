ae2425fb5934c2524b697d1dbfc27325c5985cc0
Alex Mouton <thomas.mouton@gmail.com>
2017-12-08 23:07:14 -0800

    Renamed Eff row type holes from r to e, to be closer to 'effect', as 'rows' are multipurpose, and we also have that word in our domain here.

--- docs/Data/ST/Matrix.md
+++ docs/Data/ST/Matrix.md
@@ -12,13 +12,13 @@ newtype STMat s h a
 #### `copyImpl`
 
 ``` purescript
-copyImpl :: forall a b h r. a -> Eff (st :: ST h | r) b
+copyImpl :: forall a b h e. a -> Eff (st :: ST h | e) b
 ```
 
 #### `freeze`
 
 ``` purescript
-freeze :: forall a h r. STArray h a -> Eff (st :: ST h | r) (Array a)
+freeze :: forall a h e. STArray h a -> Eff (st :: ST h | e) (Array a)
 ```
 
 Create an immutable copy of a mutable array.
@@ -26,7 +26,7 @@ Create an immutable copy of a mutable array.
 #### `thaw`
 
 ``` purescript
-thaw :: forall a h r. Array a -> Eff (st :: ST h | r) (STArray h a)
+thaw :: forall a h e. Array a -> Eff (st :: ST h | e) (STArray h a)
 ```
 
 Create a mutable copy of an immutable array.
@@ -48,55 +48,53 @@ unsafeThaw :: forall a h. Array a -> STArray h a
 #### `cloneSTMat`
 
 ``` purescript
-cloneSTMat :: forall s h a r. (STMat s h a) -> Eff (st :: ST h | r) (STMat s h a)
+cloneSTMat :: forall s h a e. (STMat s h a) -> Eff (st :: ST h | e) (STMat s h a)
 ```
 
 #### `fromSTMat`
 
 ``` purescript
-fromSTMat :: forall s h a r. Sized s => (STMat s h a) -> Eff (st :: ST h | r) (Mat s a)
+fromSTMat :: forall s h a e. Sized s => (STMat s h a) -> Eff (st :: ST h | e) (Mat s a)
 ```
 
 #### `toSTMat`
 
 ``` purescript
-toSTMat :: forall s h a r. (Mat s a) -> Eff (st :: ST h | r) (STMat s h a)
+toSTMat :: forall s h a e. (Mat s a) -> Eff (st :: ST h | e) (STMat s h a)
 ```
 
 #### `copyToSTMat`
 
 ``` purescript
-copyToSTMat :: forall s h a r. (Mat s a) -> (STMat s h a) -> Eff (st :: ST h | r) Unit
+copyToSTMat :: forall s h a e. (Mat s a) -> (STMat s h a) -> Eff (st :: ST h | e) Unit
 ```
 
 #### `identityST'`
 
 ``` purescript
-identityST' :: forall s h r. Sized s => Eff (st :: ST h | r) (STMat s h Number)
+identityST' :: forall s h e. Sized s => Eff (st :: ST h | e) (STMat s h Number)
 ```
 
 #### `scaleSTMatrixInt`
 
 ``` purescript
-scaleSTMatrixInt :: forall a h r. EuclideanRing a => a -> STArray h a -> Eff (st :: ST h | r) Unit
+scaleSTMatrixInt :: forall a h e. EuclideanRing a => a -> STArray h a -> Eff (st :: ST h | e) Unit
 ```
 
 #### `scaleSTMatrix`
 
 ``` purescript
-scaleSTMatrix :: forall s a h r. EuclideanRing a => a -> (STMat s h a) -> Eff (st :: ST h | r) (STMat s h a)
+scaleSTMatrix :: forall s a h e. EuclideanRing a => a -> (STMat s h a) -> Eff (st :: ST h | e) (STMat s h a)
 ```
 
 #### `fromMatrix`
 
 ``` purescript
-fromMatrix :: forall s h r a. Mat s a -> Eff (st :: ST h | r) (STMat s h a)
+fromMatrix :: forall s h e a. Mat s a -> Eff (st :: ST h | e) (STMat s h a)
 ```
 
 #### `runSTMatrix`
 
 ``` purescript
-runSTMatrix :: forall s a r. (forall h. Eff (st :: ST h | r) (STMat s h a)) -> Eff r (Mat s a)
+runSTMatrix :: forall s a e. (forall h. Eff (st :: ST h | e) (STMat s h a)) -> Eff e (Mat s a)
 ```
-
-
--- src/Data/ST/Matrix.purs
+++ src/Data/ST/Matrix.purs
@@ -26,14 +26,14 @@ newtype STMat s h a = STMat (STArray h a)
 
 -- try array cloning with .slice() instead of the for-loop
 -- implementation in Data.Array.ST. Needs benchmarking.
-foreign import copyImpl :: forall a b h r. a -> Eff (st :: ST h | r) b
+foreign import copyImpl :: forall a b h e. a -> Eff (st :: ST h | e) b
 
 -- | Create an immutable copy of a mutable array.
-freeze :: forall a h r. STArray h a -> Eff (st :: ST h | r) (Array a)
+freeze :: forall a h e. STArray h a -> Eff (st :: ST h | e) (Array a)
 freeze = copyImpl
 
 -- | Create a mutable copy of an immutable array.
-thaw :: forall a h r. Array a -> Eff (st :: ST h | r) (STArray h a)
+thaw :: forall a h e. Array a -> Eff (st :: ST h | e) (STArray h a)
 thaw = copyImpl
 
 -- | Freeze an ST array. Do not mutate the STArray afterwards!
@@ -41,32 +41,32 @@ foreign import unsafeFreeze :: forall a h. STArray h a -> Array a
 
 foreign import unsafeThaw :: forall a h. Array a -> STArray h a
 
-cloneSTMat :: forall s h a r. (STMat s h a) -> Eff (st :: ST h | r) (STMat s h a)
+cloneSTMat :: forall s h a e. (STMat s h a) -> Eff (st :: ST h | e) (STMat s h a)
 cloneSTMat (STMat arr) = STMat <<< unsafeThaw <$> freeze arr
 
-fromSTMat :: forall s h a r. (Sized s) => (STMat s h a) -> Eff (st :: ST h | r) (M.Mat s a)
+fromSTMat :: forall s h a e. (Sized s) => (STMat s h a) -> Eff (st :: ST h | e) (M.Mat s a)
 fromSTMat (STMat arr) = do
     x   <- freeze arr
     pure (M.fromArrayColumns x)
 
-toSTMat :: forall s h a r. (M.Mat s a) -> Eff (st :: ST h | r) (STMat s h a)
+toSTMat :: forall s h a e. (M.Mat s a) -> Eff (st :: ST h | e) (STMat s h a)
 toSTMat m = STMat <$> thaw (M.toArrayColumns m)
 
--- copyToSTMat :: forall s h a r. (M.Matrix (M.Mat s) a) => (M.Mat s a) -> (STMat s h a) -> Eff (st :: ST h | r) Unit
+-- copyToSTMat :: forall s h a e. (M.Matrix (M.Mat s) a) => (M.Mat s a) -> (STMat s h a) -> Eff (st :: ST h | e) Unit
 
-foreign import copyToSTMat :: forall s h a r. (M.Mat s a) -> (STMat s h a) -> Eff (st :: ST h | r) Unit
+foreign import copyToSTMat :: forall s h a e. (M.Mat s a) -> (STMat s h a) -> Eff (st :: ST h | e) Unit
 
-identityST' :: forall s h r. (Sized s) => Eff (st :: ST h | r) (STMat s h Number)
+identityST' :: forall s h e. (Sized s) => Eff (st :: ST h | e) (STMat s h Number)
 identityST' =
     let m = M.identity' :: M.Mat s Number
     in STMat <$> thaw (M.toArrayColumns m)
 
-foreign import scaleSTMatrixInt :: forall a h r. (EuclideanRing a) => a -> STArray h a -> Eff (st :: ST h | r) Unit
+foreign import scaleSTMatrixInt :: forall a h e. (EuclideanRing a) => a -> STArray h a -> Eff (st :: ST h | e) Unit
 
-scaleSTMatrix :: forall s a h r. (EuclideanRing a) => a -> (STMat s h a) -> Eff (st :: ST h | r) (STMat s h a)
+scaleSTMatrix :: forall s a h e. (EuclideanRing a) => a -> (STMat s h a) -> Eff (st :: ST h | e) (STMat s h a)
 scaleSTMatrix x v@(STMat arr) = scaleSTMatrixInt x arr *> pure v
 
-fromMatrix :: forall s h r a. M.Mat s a -> Eff (st :: ST h | r) (STMat s h a)
+fromMatrix :: forall s h e a. M.Mat s a -> Eff (st :: ST h | e) (STMat s h a)
 fromMatrix (M.Mat m) = STMat <$> thaw m
 
-foreign import runSTMatrix :: forall s a r. (forall h. Eff (st :: ST h | r) (STMat s h a)) -> Eff r (M.Mat s a)
+foreign import runSTMatrix :: forall s a e. (forall h. Eff (st :: ST h | e) (STMat s h a)) -> Eff e (M.Mat s a)
