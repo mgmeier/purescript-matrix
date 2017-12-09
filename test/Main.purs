bfecb99486b82d1e91d2d4e364d978fa85afcce2
Alex Mouton <thomas.mouton@gmail.com>
2017-12-08 22:47:51 -0800

    Put DIY tests into a sidebar and added purescript-spec to dev dependencies.

--- test/Main.purs
+++ test/Main.purs
@@ -5,6 +5,11 @@ import Prelude
 import Data.Matrix4 (Mat4, Vec3N, identity, translate, rotate) as M
 import Data.ST.Matrix4 (STMat4, translateST, rotateST, identityST) as M
 import Data.ST.Matrix (runSTMatrix) as M
+
+import Test.Spec (pending)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (RunnerEffects, run)
+
 import Data.Vector as V
 
 import Control.Monad.Eff (Eff)
@@ -26,14 +31,17 @@ ys = M.rotate 90.0 meh $ M.identity
 zs :: M.Mat4
 zs = M.translate meh $ M.identity
 
-main ::  forall eff. Eff (console :: CONSOLE | eff) Unit
-main = do
+preexisting :: forall e. Eff ( console :: CONSOLE | e ) Unit
+preexisting = do
+  xs <- M.runSTMatrix (ble >>= \m -> M.rotateST 90.0 meh m *> pure m)
+  bs <- M.runSTMatrix (ble >>= \m -> M.translateST meh m *> pure m)
 
-    xs <- M.runSTMatrix (ble >>= \m -> M.rotateST 90.0 meh m *> pure m)
-    bs <- M.runSTMatrix (ble >>= \m -> M.translateST meh m *> pure m)
+  logShow xs
+  logShow ys
 
-    logShow xs
-    logShow ys
+  logShow bs
+  logShow zs
 
-    logShow bs
-    logShow zs
+main :: Eff (RunnerEffects ()) Unit
+main = run [consoleReporter] do
+  pending "tests"
